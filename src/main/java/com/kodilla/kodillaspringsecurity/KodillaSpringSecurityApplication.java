65780237ace9550a5731f19e3ac4dfdee73733b9
Artur Krawczyk <artur.damian.krawczyk@gmail.com>
2022-11-13 16:43:39 +0100

    first commit

---
+++ src/main/java/com/kodilla/kodillaspringsecurity/KodillaSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillaspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaSpringSecurityApplication.class, args);
+    }
+
+}
